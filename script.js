3d753f4dede510723c2d8b9dfc7c46754d582e19
Bruno Fernando Candido <candido10bruno@gmail.com>
2023-05-27 13:44:46 -0300

    show_data_from_firebase

--- script.js
+++ script.js
@@ -26,7 +26,6 @@ function cadastrar() {
             "km": {
                 "stringValue": km
             }
-
         }
     }
 
@@ -36,11 +35,15 @@ function cadastrar() {
     }
 
     fetch("https://firestore.googleapis.com/v1/projects/cadastro-de-carros-573be/databases/(default)/documents/Carros", options)
-        .then(T => {
-            console.log(T.json);
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let listaDados = document.getElementById("lista-dados");
+            let itemLista = document.createElement("li");
+            itemLista.innerText = `Marca: ${marca}\nModelo: ${modelo}\nCor: ${cor}\nPlaca: ${placa}\nAno: ${ano}\nKm: ${km}\n`;
+            listaDados.appendChild(itemLista);
         })
-        .catch(e => {
-            console.error(e);
+        .catch(error => {
+            console.error(error);
         });
-
 }
