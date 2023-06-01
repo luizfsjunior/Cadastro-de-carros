d552e198425e603886f4887528cfe121e12c9cb0
Luiz Fernando <luizfsjunior.2002@gmail.com>
2023-06-01 00:15:16 -0300

    Construção da tabela/GET

---
+++ scripts/script.js
@@ -0,0 +1,148 @@
+function deletar(id) {
+    const options = {
+        method: 'DELETE'
+    }
+
+    fetch("https://firestore.googleapis.com/v1/projects/cadastro-de-carros-573be/databases/(default)/documents/Carros/" + id, options)
+        .then(t => {
+            console.log(id);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+function cadastrar(id, method) {
+    var marca = document.getElementById("marca").value;
+    var modelo = document.getElementById("modelo").value;
+    var cor = document.getElementById("cor").value;
+    var placa = document.getElementById("placa").value;
+    var ano = document.getElementById("ano").value;
+    var km = document.getElementById("km").value;
+
+    let jsonBody = {
+        "fields": {
+            "marca": {
+                "stringValue": marca
+            },
+            "modelo": {
+                "stringValue": modelo
+            },
+            "cor": {
+                "stringValue": cor
+            },
+            "placa": {
+                "stringValue": placa
+            },
+            "ano": {
+                "integerValue": ano
+            },
+            "km": {
+                "stringValue": km
+            }
+        }
+    }
+    const options = {
+        method: method,
+        body: JSON.stringify(jsonBody)
+    }
+
+    fetch("https://firestore.googleapis.com/v1/projects/cadastro-de-carros-573be/databases/(default)/documents/Carros/" + id, options)
+        .then(t => {
+            console.log(id);
+        })
+        .then(() => {
+            // Limpar os campos do formulário
+            document.getElementById("marca").value = "";
+            document.getElementById("modelo").value = "";
+            document.getElementById("cor").value = "";
+            document.getElementById("placa").value = "";
+            document.getElementById("ano").value = "";
+            document.getElementById("km").value = "";
+
+            // Atualizar a tabela com os novos dados
+            busca();
+        })
+        //.then(busca)
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+
+
+function criarBotaoEditar(index) {
+    let botaoEditar = document.createElement("button");
+    botaoEditar.textContent = "Editar";
+
+    botaoEditar.addEventListener("click", () => {
+        let div_edit = document.getElementById("editar");
+        div_edit.style.display = "flex"
+        let btnCadastro = document.getElementById("cadastro-btn");
+        btnCadastro.style.display = "none"
+
+        let btnEdit = document.getElementById("edit-btn");
+        btnEdit.addEventListener("click", cadastrar(index, 'PATCH'));
+
+        let btnDelete = document.getElementById("delete-btn");
+        btnEdit.addEventListener("click", deletar(index));
+
+
+    });
+    return botaoEditar;
+}
+
+
+function busca() {
+    let corpoTabela = document.getElementById("corpoTabela");
+    fetch("https://firestore.googleapis.com/v1/projects/cadastro-de-carros-573be/databases/(default)/documents/Carros")
+        .then(response => response.json())
+        .then(carros => {
+
+            corpoTabela.innerHTML = "";
+
+            let n = carros.documents.length;
+            console.log(n);
+            let i;
+
+            for (i = 0; i < n; i++) {
+
+                c = carros.documents[i];
+                id = c.name.split('/')[6];
+                let novaLinha = document.createElement("TR")
+                corpoTabela.appendChild(novaLinha);
+
+                let colMarca = document.createElement("TD")
+                let colModelo = document.createElement("TD")
+                let colCor = document.createElement("TD")
+                let colPlaca = document.createElement("TD")
+                let colKm = document.createElement("TD")
+                let colAno = document.createElement("TD")
+                let colEditar = document.createElement("TD");
+
+
+
+                novaLinha.appendChild(colMarca);
+                novaLinha.appendChild(colModelo);
+                novaLinha.appendChild(colCor);
+                novaLinha.appendChild(colPlaca);
+                novaLinha.appendChild(colKm);
+                novaLinha.appendChild(colAno);
+                novaLinha.appendChild(colEditar);
+
+                colEditar.appendChild(criarBotaoEditar(id));
+                colMarca.innerHTML = c.fields.marca.stringValue;
+                colModelo.innerHTML = c.fields.modelo.stringValue;
+                colCor.innerHTML = c.fields.cor.stringValue;
+                colPlaca.innerHTML = c.fields.placa.stringValue;
+                colKm.innerHTML = c.fields.km.stringValue;
+                colAno.innerHTML = c.fields.ano.integerValue;
+            }
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+
+window.addEventListener('load', busca);
