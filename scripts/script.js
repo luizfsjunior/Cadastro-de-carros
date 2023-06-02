8c52e2412e72ab01f4b923edaeb90920e09c8dd4
Luiz Fernando <luizfsjunior.2002@gmail.com>
2023-06-02 17:59:35 -0300

    correção de bug botão voltar

--- scripts/script.js
+++ scripts/script.js
@@ -112,6 +112,7 @@ function criarBotaoEditar(index) {
 
                 let btnVolta = document.getElementById("voltar-btn");
                 btnVolta.addEventListener("click", function () {
+                    limpa_form();
                     div_edit.style.display = "none";
                     btnCadastro.style.display = "block";
                 });
